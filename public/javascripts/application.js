97f9e5d486c81be2b2cda8f26251b172f88b5c2b
jana4u <jana4u@seznam.cz>
2011-11-03 18:41:52 +0100

    ajax loader

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,16 @@
 // Put your application scripts here
+
+jQuery(function($) {
+  $("form[data-remote=true]").ajaxStart(function() {
+    $('#error').html("");
+    $("#ajax-loader").show();
+  });
+
+  $("form[data-remote=true]").ajaxError(function(e, xhr, settings, exception) {
+    $('#error').html("Server Error - " + xhr.status);
+  });
+
+  $("form[data-remote=true]").ajaxStop(function() {
+    $("#ajax-loader").hide();
+  });
+});
