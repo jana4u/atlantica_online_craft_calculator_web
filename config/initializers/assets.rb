51c2954c0e956d6ea40080266b7e9e39507d4afe
Jana Dvořáková <jana4u@seznam.cz>
2021-06-14 17:03:47 +0200

    precompile image from engine

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w(atlantica_online_craft_calculator_engine/ajax-loader.gif)
