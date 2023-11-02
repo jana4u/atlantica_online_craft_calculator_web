c726c2084d6e709e16b62287c173609a557e5d69
Jana Dvořáková <jana4u@seznam.cz>
2023-11-02 16:39:46 +0100

    RuboCop style fixes

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.2.2'
+ruby "3.2.2"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
-gem 'rails', '~> 6.1.7'
+gem "rails", "~> 6.1.7"
 # Use SCSS for stylesheets
-gem 'sass-rails', '>= 6'
+gem "sass-rails", ">= 6"
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -14,34 +14,34 @@ gem 'sass-rails', '>= 6'
 gem "unicorn-rails"
 
 # Twitter Bootstrap 2.x
-gem 'bootstrap-sass', '< 3'
+gem "bootstrap-sass", "< 3"
 
 # Use jquery as the JavaScript library
-gem 'jquery-rails'
+gem "jquery-rails"
 
 # Delivering exception notifications by email
-gem 'exception_notification'
+gem "exception_notification"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
 end
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
-  gem 'web-console', '>= 4.1.0'
+  gem "web-console", ">= 4.1.0"
   # Display performance information such as SQL time and flame graphs for each request in your browser.
   # Can be configured to work on production as well see: https://github.com/MiniProfiler/rack-mini-profiler/blob/master/README.md
-  gem 'rack-mini-profiler', '~> 2.0'
+  gem "rack-mini-profiler", "~> 2.0"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :development do
   gem "standard", require: false
 end
 
 # Atlantica Online Craft Calculator
-gem 'atlantica_online_craft_calculator_engine', github: 'jana4u/atlantica_online_craft_calculator_engine'
-gem 'atlantica_online_craft_calculator', github: 'jana4u/atlantica_online_craft_calculator'
+gem "atlantica_online_craft_calculator_engine", github: "jana4u/atlantica_online_craft_calculator_engine"
+gem "atlantica_online_craft_calculator", github: "jana4u/atlantica_online_craft_calculator"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -45,11 +45,10 @@ Rails.application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -72,9 +71,9 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Inserts middleware to perform automatic connection switching.
@@ -100,26 +99,26 @@ Rails.application.configure do
 
   # Action Mailer host setting
   config.action_mailer.default_url_options = {
-    host: 'craftcalculator.jana4u.net'
+    host: "craftcalculator.jana4u.net"
   }
 
   # Action Mailer Configuration
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: ENV['SMTP_ADDRESS'],
-    port: ENV['SMTP_PORT'],
-    domain: ENV['SMTP_DOMAIN'],
-    user_name: ENV['SMTP_USER_NAME'],
-    password: ENV['SMTP_PASSWORD'],
-    authentication: 'plain',
+    address: ENV["SMTP_ADDRESS"],
+    port: ENV["SMTP_PORT"],
+    domain: ENV["SMTP_DOMAIN"],
+    user_name: ENV["SMTP_USER_NAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: "plain",
     enable_starttls_auto: true
   }
 
   # ExceptionNotifier
   config.middleware.use ExceptionNotification::Rack,
     email: {
-      email_prefix: '[CraftCalculator Error] ',
-      sender_address: %{"ExceptionNotifier" <#{ENV['EMAIL_SENDER']}>},
-      exception_recipients: ENV['EMAIL_RECIPIENT']
+      email_prefix: "[CraftCalculator Error] ",
+      sender_address: %("ExceptionNotifier" <#{ENV["EMAIL_SENDER"]}>),
+      exception_recipients: ENV["EMAIL_RECIPIENT"]
     }
 end
