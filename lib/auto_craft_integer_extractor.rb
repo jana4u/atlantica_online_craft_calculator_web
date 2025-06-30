3dfa89305e3007415d8aa2b71487b91eed35dcab
Jana Dvořáková <jana4u@seznam.cz>
2025-06-30 18:33:59 +0200

    Fixing Style/ComparableClamp offense

--- lib/auto_craft_integer_extractor.rb
+++ lib/auto_craft_integer_extractor.rb
@@ -1,5 +1,5 @@
 module AutoCraftIntegerExtractor
   def self.non_negative_integer_from_string(string)
-    [[IntegerExtractor.non_negative_integer_from_string(string), 1].max, 120].min
+    IntegerExtractor.non_negative_integer_from_string(string).clamp(1, 120)
   end
 end
