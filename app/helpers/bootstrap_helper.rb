570ab8b5fb428cc2479a18aabd08d76ac0f6fb8c
Jana Dvořáková <jana4u@seznam.cz>
2025-07-01 17:14:47 +0200

    Upgrading to Bootstrap 5.3 - layout

---
+++ app/helpers/bootstrap_helper.rb
@@ -0,0 +1,13 @@
+module BootstrapHelper
+  def navbar_link_to(name = nil, options = nil, html_options = nil, &block)
+    html_options ||= {}
+    html_options[:class] ||= "nav-link"
+
+    if current_page?(options)
+      html_options[:class] += " active"
+      html_options[:"aria-current"] = "page"
+    end
+
+    link_to(name, options, html_options, &block)
+  end
+end
