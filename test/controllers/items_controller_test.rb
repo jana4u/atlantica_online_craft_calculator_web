7aa3fa217d63590f7b3f3c2bff1310c670325bcb
Jana Dvořáková <jana4u@seznam.cz>
2025-07-03 16:56:22 +0200

    Better tests for calculator

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -5,30 +5,35 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
     get root_path
 
     assert_response :success
+    assert_no_item_craft
   end
 
   test "index with skill" do
     get root_path(skill: "Action")
 
     assert_response :success
+    assert_no_item_craft
   end
 
   test "index with search" do
     get root_path(query: "craft")
 
     assert_response :success
+    assert_no_item_craft
   end
 
   test "index with item and no count" do
     get root_path(item_name: craftable_item_name)
 
     assert_response :success
+    assert_item_craft
   end
 
   test "index with item and count" do
     get root_path(item_name: craftable_item_name, count: "10")
 
     assert_response :success
+    assert_item_craft
   end
 
   test "index with item and custom prices" do
@@ -41,5 +46,16 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
     get root_path(item_name: craftable_item_name)
 
     assert_response :success
+    assert_item_craft
+  end
+
+  private
+
+  def assert_item_craft
+    assert_dom "h1", count: 1, text: craftable_item_name
+  end
+
+  def assert_no_item_craft
+    assert_dom "h1", count: 0
   end
 end
